68cc5ff2ce3ea51ebed06a2c640df863fdb4d9a1
Raghav <raghav@Raghav.local>
2020-04-27 05:09:35 -0700

    V-1.3

---
+++ mastersfriend/build/precache-manifest.c1eee11bbd9709837567964f88072be0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a649932ec0b2f0d5bfddb2f5e65695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb50d6ce0f2fd3b8f6d8",
+    "url": "/static/css/2.a5b4ddd2.chunk.css"
+  },
+  {
+    "revision": "b6cea882d91fa56ca034",
+    "url": "/static/css/main.2e5fa87a.chunk.css"
+  },
+  {
+    "revision": "fb50d6ce0f2fd3b8f6d8",
+    "url": "/static/js/2.e13220d9.chunk.js"
+  },
+  {
+    "revision": "4e8d24b2339bfb46ff7da8bf8bd2598d",
+    "url": "/static/js/2.e13220d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cea882d91fa56ca034",
+    "url": "/static/js/main.e373630b.chunk.js"
+  },
+  {
+    "revision": "80ee17035e317b83abf5",
+    "url": "/static/js/runtime-main.2fac61c3.js"
+  }
+]);
---
+++ mastersfriend/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LandingPage = () => (
+  <div>
+    <h1>Friend for your Master's</h1>
+    <p>This Web App supports 5 colleges as of now.</p>
+    <p>This page will contain information about Weather, Financial Aid, Location and also about Graduate Computer Science Department of 5 Universities.</p>
+    <p>The University Suggestor is also Under Construction..</p>
+    <p>This page is public, accessible by everyone.</p>
+
+  </div>
+);
+
+export default LandingPage;
