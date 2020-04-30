aae7f173d662be5134007f27caa47499e0b7bb84
nithishb <nithish.mdh@gmail.com>
2020-04-30 01:01:16 -0700

    Updated1

--- frontend/src/old_components/SignOut.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Button from '@material-ui/core/Button';
-import { auth } from "../firebase";
-
-const SignOutButton = () => (
-  <Button  variant="contained" color="secondary" onClick={auth.doSignOut}>Sign Out </Button> 
-);
-
-export default SignOutButton;
